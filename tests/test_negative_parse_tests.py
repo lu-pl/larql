2d5dca0cfe2c5e4b864b1f4a23747d3dfe920f64
Lukas Plank <lupl@tuta.io>
2025-04-20 09:32:55 +0200

    test: use larql.SPARQLParser for tests

--- tests/test_negative_parse_tests.py
+++ tests/test_negative_parse_tests.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-from lark import LarkError
-from larql import SPARQLParser, sparql_parser
 import pytest
 
+from lark import LarkError
+from larql import SPARQLParser
 from tests.discovery import SPARQL11SyntaxTestDiscovery
 
 
@@ -14,9 +14,6 @@ negative_test_paths = SPARQL11SyntaxTestDiscovery().get_syntax_tests("negative")
 def test_positive_query_parsing(test_query_path):
     _query_file_path: Path = Path(test_query_path)
     query: str = _query_file_path.read_text()
-
-    with pytest.raises(LarkError):
-        sparql_parser.parse(query)
 
     with pytest.raises(LarkError):
         SPARQLParser(query)
