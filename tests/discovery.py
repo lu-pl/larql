50886100455b6231f866d6c610cb744039dfd854
Lukas Plank <lupl@tuta.io>
2026-09-25 11:49:14 +0200

    test(w3c): rewrite W3C test discovery
    The change introduces a rewrite of the W3C test discovery logic.
    
    Given an initial manifest file, the disovery utility now recursively scans the entire test suite
    subtree and lazily produces `SyntaxTestInfo` objects that hold the path and validity info about each test.
    
    Closes #15.

--- tests/discovery.py
+++ tests/discovery.py
@@ -1,81 +1,93 @@
-"""Test utility for rdf-test syntax test discovery."""
-
-from collections.abc import Iterator
-from itertools import chain
+from collections.abc import Iterable, Iterator
+from dataclasses import dataclass
+from functools import cached_property
 from pathlib import Path
-from string import Template
-from typing import Literal as TypingLiteral
 from urllib.parse import urlparse
 
-from rdflib import Graph
+from rdflib import Graph, URIRef
+from sparqlx import SPARQLWrapper
 
 
-class SPARQL11SyntaxTestDiscovery:
-    def __init__(self):
-        self._manifest_all_file = Path(
-            "larql/tests/rdf-tests/sparql/sparql11/manifest-all.ttl"
-        )
+@dataclass
+class SyntaxTestInfo:
+    test_id: URIRef
+    test_path: Path
+    valid: bool
 
-    def _get_syntax_manifests(self) -> Iterator:
-        manifest_all_graph: Graph = Graph().parse(self._manifest_all_file)
 
-        query = """
-        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+class SyntaxTestDiscovery(Iterable[SyntaxTestInfo]):
+    def __init__(self, manifest: Path) -> None:
+        self._manifest = manifest
+
+    def __iter__(self) -> Iterator[SyntaxTestInfo]:
+        yield from self.entries
+
+        for manifest in self.manifests:
+            yield from SyntaxTestDiscovery(manifest=manifest)
+
+    @cached_property
+    def manifest_graph(self) -> Graph:
+        return Graph().parse(source=self._manifest)
+
+    @property
+    def manifests(self) -> Iterator[Path]:
+        manifest_discovery_query = """
         prefix mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#>
-
-        select distinct ?element
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+        
+        select ?manifest
         where {
-            ?collection rdf:rest*/rdf:first ?element .
-            filter regex(str(?element), '/syntax-.+/manifest.ttl$')
+          [ mf:include/rdf:rest*/rdf:first ?manifest ] .
+        }
+        """
+        wrapper = SPARQLWrapper(sparql_endpoint=self.manifest_graph)
+        manifests: list[dict] = wrapper.query(manifest_discovery_query, convert=True)
+
+        for manifest in manifests:
+            path_uri: URIRef = manifest["manifest"]
+            yield self._pathify_uri(path_uri=path_uri)
+
+    @property
+    def entries(self) -> Iterator[SyntaxTestInfo]:
+
+        syntax_test_discovery_query = """
+        prefix mf:  <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#>
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+        select ?test_id ?test_path_uri ?valid
+        where {
+        values (?test_type ?validity) {
+          (mf:PositiveSyntaxTest         true)
+          (mf:PositiveUpdateSyntaxTest   true)
+          (mf:PositiveSyntaxTest11       true)
+          (mf:PositiveUpdateSyntaxTest11 true)
+
+          (mf:NegativeSyntaxTest         false)
+          (mf:NegativeUpdateSyntaxTest   false)
+          (mf:NegativeSyntaxTest11       false)
+          (mf:NegativeUpdateSyntaxTest11 false)
+        }
+
+        [ mf:entries/rdf:rest*/rdf:first ?test_id ] .
+
+        ?test_id a ?test_type ;
+          mf:action ?test_path_uri .
         }
         """
 
-        sparql_result = manifest_all_graph.query(query)
-        yield from chain.from_iterable(sparql_result)  # type: ignore
+        wrapper = SPARQLWrapper(sparql_endpoint=self.manifest_graph)
+        entries: list[dict] = wrapper.query(syntax_test_discovery_query, convert=True)
+
+        for entry in entries:
+            test_id: URIRef = entry["test_id"]
+            test_path_uri: URIRef = entry["test_path_uri"]
+            valid: bool = entry["valid"]
+
+            test_path: Path = self._pathify_uri(path_uri=test_path_uri)
+
+            yield SyntaxTestInfo(test_id=test_id, test_path=test_path, valid=valid)
 
     @staticmethod
-    def _get_test_type(
-        test_type: TypingLiteral["positive", "negative"],
-    ) -> tuple[str, str]:
-        match test_type:
-            case "positive":
-                return ("PositiveSyntaxTest11", "PositiveUpdateSyntaxTest11")
-            case "negative":
-                return ("NegativeSyntaxTest11", "NegativeUpdateSyntaxTest11")
-            case _:
-                raise Exception(
-                    "Applicable values for parameter test_type are 'positive' or 'negative'."
-                )
-
-    def get_syntax_tests(self, test_type: TypingLiteral["positive", "negative"]):
-        type_class_1, type_class_2 = self._get_test_type(test_type)
-        _query_template = Template(
-            """
-            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            prefix mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#>
-
-            select ?action
-            where {
-                {?test a mf:$type_class_1.}
-                union
-                {?test a mf:$type_class_2.}
-
-            ?test mf:action ?action .
-            }
-            """
-        )
-
-        query = _query_template.substitute(
-            type_class_1=type_class_1, type_class_2=type_class_2
-        )
-
-        for manifest in self._get_syntax_manifests():
-            graph = Graph().parse(manifest)
-            sparql_result = graph.query(query)
-
-            yield from map(
-                lambda x: urlparse(x).path,
-                chain.from_iterable(sparql_result),  # type: ignore
-            )
+    def _pathify_uri(path_uri: URIRef) -> Path:
+        parsed = urlparse(path_uri)
+        return Path(parsed.path)
